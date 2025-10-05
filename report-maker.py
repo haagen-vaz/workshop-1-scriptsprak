6e9b0acf206821b3c8a3b82a31c7b4bf37c41014
haagen-vaz <vasko1989@hotmail.com>
2025-10-05 12:57:06 +0200

    print site location and each location

--- report-maker.py
+++ report-maker.py
@@ -1,1 +1,9 @@
-print ('hello vasko')
+# Import the json library so that we can handle json
+
+import json
+data = json.load(open("network.devices.json","r",encoding = "utf-8"))
+
+for location in data ["locations"]:
+    print (location["site"])
+    for device in location ["devices"]:
+        print (device["hostname"])
